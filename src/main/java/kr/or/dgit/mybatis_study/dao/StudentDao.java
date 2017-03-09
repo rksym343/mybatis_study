271fbc90109ac5fd54c1cdd2f06ea8261223b0d8
lkm <rksym343@naver.com>
2017-03-09 12:31:35 +0900

    test

--- src/main/java/kr/or/dgit/mybatis_study/dao/StudentDao.java
+++ src/main/java/kr/or/dgit/mybatis_study/dao/StudentDao.java
@@ -7,4 +7,10 @@ import kr.or.dgit.mybatis_study.dto.Student;
 public interface StudentDao {
 	int insertStudent(Student student);
 	List<Student> selectStudentByAll();
+	
+	// 과제 test까지 완료~ 깃허브 올리고~ 브랜치 이용해서 해야함다~
+	Student selectStudentByNo(int studNo);
+	//int updateStudent(Student student);
+	//int deleteStudentByNo(int StudNo);
+	
 }
--- src/test/java/kr/or/dgit/mybatis_study/StudentDaoTest.java
+++ src/test/java/kr/or/dgit/mybatis_study/StudentDaoTest.java
@@ -37,7 +37,7 @@ public class StudentDaoTest {
 	
 	@Test
 	public void testInsertStudent() {
-		Student std = new Student(5, "조시은", "jsw@test.co.kr", new Date());
+		Student std = new Student(6, "조시은", "jsw@test.co.kr", new Date());
 		int res = studentService.insertStudent(std);
 		Assert.assertEquals(1, res);
 	}
