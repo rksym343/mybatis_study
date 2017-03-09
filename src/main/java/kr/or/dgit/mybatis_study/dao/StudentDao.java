5f65d28cc8ccbb0f7ee219813a012700209b7d2c
lkm <rksym343@naver.com>
2017-03-09 10:48:27 +0900

    studentdao->selectStudentByAll

---
+++ src/main/java/kr/or/dgit/mybatis_study/dao/StudentDao.java
@@ -0,0 +1,10 @@
+package kr.or.dgit.mybatis_study.dao;
+
+import java.util.List;
+
+import kr.or.dgit.mybatis_study.dto.Student;
+
+public interface StudentDao {
+	// int insertStudent(Student student);
+	List<Student> selectStudentByAll();
+}
